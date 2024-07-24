28324bc9477dfab477560d4811a63647e23f9418
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-07-24 15:42:44 +0100

    App Setting JSON File modification (adding doctor rank) + Adding User Model

---
+++ Hakim/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Update;
+
+namespace Hakim.Model
+{
+    public class User
+    {
+        private string rank { get; set; }
+        private string lastName { get; set; }
+        private string firstName { get; set; }
+    }
+
+}
