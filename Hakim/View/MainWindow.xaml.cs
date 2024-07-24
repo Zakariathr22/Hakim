ecf55b10d9464baa7f00056ea8ed2f719b98cfd3
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-07-24 11:12:38 +0100

    Main Window -> Set window properties, center on screen

--- Hakim/View/MainWindow.xaml.cs
+++ Hakim/View/MainWindow.xaml.cs
@@ -1,3 +1,5 @@
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
+using Windows.UI;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +27,74 @@ namespace Hakim
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private AppWindow appWindow;
+        private OverlappedPresenter overlappedPresenter;
+        private AppWindowTitleBar titleBar;
+        private bool isActivatedOnce = false;
         public MainWindow()
         {
             this.InitializeComponent();
+
+            appWindow = GetAppWindowForCurrentWindow();
+            overlappedPresenter = GetAppWindowOverlappedPresenter(appWindow);
+            titleBar = GetAppWindowTitleBar(appWindow);
+            titleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+
+            titleBar.ButtonForegroundColor = Color.FromArgb(0, 128, 128, 128);
+
+            //appWindow.Resize(new Windows.Graphics.SizeInt32(1130, 480));
+            appWindow.Title = "Hakim";
+            appWindow.SetIcon("Assets/Icons/Hakim.ico");
+            //overlappedPresenter.Maximize();
+            //navigationView.Height = "auto";
+
+            titleBar.ExtendsContentIntoTitleBar = true;
+
+            CenterWindow();
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(myWndId);
+        }
+
+        private OverlappedPresenter GetAppWindowOverlappedPresenter(AppWindow appWindow)
+        {
+            return (OverlappedPresenter)appWindow.Presenter;
+        }
+
+        private AppWindowTitleBar GetAppWindowTitleBar(AppWindow appWindow)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = appWindow.TitleBar;
+
+                return titleBar;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void CenterWindow()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            if (appWindow is not null)
+            {
+                Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                if (displayArea is not null)
+                {
+                    var CenteredPosition = appWindow.Position;
+                    CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
+                    CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
+                    appWindow.Move(CenteredPosition);
+                }
+            }
         }
     }
 }
