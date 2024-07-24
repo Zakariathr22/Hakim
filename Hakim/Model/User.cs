4f56872bf0bbe993e1673ebad7930efc5962af90
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-07-24 15:50:31 +0100

    Modify user class by adding calculated fullname (INotifyPropertyChanged)

--- Hakim/Model/User.cs
+++ Hakim/Model/User.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.System.Update;
 
 namespace Hakim.Model
 {
-    public class User
+    public class User : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         private string rank { get; set; }
+        public string Rank
+        {
+            get => rank;
+            set
+            {
+                rank = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(profitionalName));
+            }
+        }
         private string lastName { get; set; }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(profitionalName));
+            }
+        }
         private string firstName { get; set; }
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(profitionalName));
+            }
+        }
+        public string profitionalName
+        {
+            get
+            {
+                return $"{rank} {lastName} {firstName}";
+            }
+        }
+        void OnPropertyChanged([CallerMemberName] string PropertyName = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
     }
-
 }
