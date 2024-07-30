5dfb16bb6385b97b53809b57bf78c9b73a7e57c7
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-07-30 16:23:47 +0100

    Patients -> Design Review -> Setting colors of Patient cards using theme resources Dinamically

--- Hakim/View/Clients/PatientCardControl.xaml.cs
+++ Hakim/View/Clients/PatientCardControl.xaml.cs
@@ -41,6 +41,8 @@ namespace Hakim.View.Clients
                 // You now have access to the parent page
                 Debug.WriteLine("Parent Page found: " + ParentPage.GetType().Name);
             }
+
+            SetCardColor();
         }
 
         private void ShowMenu()
@@ -73,6 +75,51 @@ namespace Hakim.View.Clients
             {
                 // Now you have access to the associated Patient object
                 ParentPage.ShowEditPatientDialog(patient);
+            }
+        }
+
+        private void CustomButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext is Patient patient)
+            {
+                App.mainWindow.contentFrame.Navigate(typeof(PatientPage), patient);
+            }
+        }
+
+        private void SetCardColor()
+        {
+            if (this.DataContext is Patient patient)
+            {
+                if (patient.id % 4 == 0)
+                {
+                    var Background = (Border)this.Resources["GreenBackground"];
+                    var Foreground = (Border)this.Resources["GreenForeground"];
+
+                    personPicture.Background = Background.Background;
+                    fullNameAndAgeTextBlock.Foreground = Foreground.Background;
+                }
+                else if (patient.id % 3 == 0)
+                {
+                    var Background = (Border)this.Resources["AmberBackground"];
+                    var Foreground = (Border)this.Resources["AmberForeground"];
+
+                    personPicture.Background = Background.Background;
+                    fullNameAndAgeTextBlock.Foreground = Foreground.Background;
+                }
+                else if (patient.id % 2 == 0)
+                {
+                    var Background = (Border)this.Resources["RedBackground"];
+                    var Foreground = (Border)this.Resources["RedForeground"];
+
+                    personPicture.Background = Background.Background;
+                    fullNameAndAgeTextBlock.Foreground = Foreground.Background;
+                }
+                else
+                {
+                    var Background = (Border)this.Resources["AccentBackground"];
+
+                    personPicture.Background = Background.Background;
+                }
             }
         }
     }
