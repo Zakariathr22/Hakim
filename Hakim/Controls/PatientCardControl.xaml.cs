51c739e2ea096c1a62fdd80f4c8806a8221da167
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-03-04 17:01:35 +0100

    Refactor PatientCardControl into Hakim.Controls namespace
    This commit refactors the PatientCardControl into a new namespace, Hakim.Controls, updating both the XAML and code-behind files accordingly. The UserControl declaration has been modified to reflect the new namespace, and additional UI elements such as CommandBarFlyout and AppBarButton have been added for improved functionality. The code-behind file now includes necessary using directives for the new namespace, ensuring proper functionality with the updated UI elements. Additionally, PatientsPage.xaml has been updated to reference the new PatientCardControl. These changes enhance code organization and maintainability.

--- Hakim/Controls/PatientCardControl.xaml.cs
+++ Hakim/Controls/PatientCardControl.xaml.cs
@@ -1,28 +1,12 @@
-using Hakim.Models;
 using Hakim.Services;
-using Hakim.ViewModels;
+using Hakim.Views.Patients;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using static System.Net.Mime.MediaTypeNames;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace Hakim.Views.Patients
+namespace Hakim.Controls
 {
     public sealed partial class PatientCardControl : UserControl
     {
